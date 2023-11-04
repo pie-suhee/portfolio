3e0dcd73c88b0a0e817c8d6af7c594cb22d58343
pie-suhee <120539985+pie-suhee@users.noreply.github.com>
2023-11-04 12:57:57 +0900

    20231104

--- reactshop/src/components/Drawer.tsx
+++ reactshop/src/components/Drawer.tsx
@@ -10,7 +10,7 @@ const Drawer = ({ closeOverlay }: any): JSX.Element => {
   return (
     <div className='drawer-side'>
       <label htmlFor='side-menu' className='drawer-overlay'></label>
-      <ul className='menu w-60 sm:w-80 p-4 overflow-y-auto bg-white dark:bg-base-100'>
+      <ul className='menu w-60 sm:w-80 mt-16 p-4 overflow-y-auto bg-white dark:bg-base-100'>
         {menus.map((menu) => {
           return (
             <li key={menu.name}>
